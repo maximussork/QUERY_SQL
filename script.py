6b053e43c4528a65ff7a2d559c59e406064bf971
maximussork <119592596+maximussork@users.noreply.github.com>
2022-12-05 14:09:05 +0100

    Add files via upload
    Script en teoria finalizado.

--- script.py
+++ script.py
@@ -1,11 +1,18 @@
-from datetime import datetime
-tiempo = 
+import datetime
+import logging
 
+arg0 = "> put"
+arg1 = "Successfully"
+dt = datetime.datetime.now()
+exec_time = dt.strftime("%d/%m/%Y %H:%M")
+logging.basicConfig(filename="log.txt", level=logging.ERROR)
+logerror_msg = "Execution time: " + exec_time + " - Error en SalidaFTP.txt. Por favor vuelva a intentarlo."
 
-with open(r'SalidaFTP.txt', 'r') as file:
-    content = file.read()
-    if '> put' in content:
-        print('Si')
-        print tiempo
+with open('SalidaFTP.txt') as file:
+    ftp = file.read()
+    if arg0 in ftp and arg1 in ftp:
+        print('Congrats.')
+        print ("Execution time: " + exec_time)
     else:
-        print('No')
+        print ("Try again!")
+        logging.error(logerror_msg)
